4740de0d73cc407b72331086aa22be1a84677d07
Omnious <panjigemilang31298@gmail.com>
2019-08-26 20:15:51 +0700

    prepare to deploy. trying to fix invalid Header Host. Beta_v.1.2.x

--- client/server.js
+++ client/server.js
@@ -1,8 +1,18 @@
+const express = require("express")
 const proxy = require("http-proxy-middleware")
+const path = require("path")
+
+const app = express()
 
 module.exports = function(app) {
-  app.use(proxy("/api/**", { target: "https://api-kampungdesa.herokuapp.com" }))
-  app.use(
-    proxy("/otherApi/**", { target: "https://api-kampungdesa.herokuapp.com" })
-  )
+  app.use(proxy("/api/*", { target: "https://api-kampungdesa.herokuapp.com" }))
+}
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("/build"))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"))
+  })
 }
